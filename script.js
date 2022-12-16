cc370fbf141ddf46f7ad76e4cbb7901838a59596
Arthur-Le-M <arthurlemenn29870@gmail.com>
2022-12-16 13:11:48 +0100

    Refonte du front

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const articles = document.querySelectorAll('.article');
+images = document.querySelectorAll('.articleImg');
+images.forEach(image => {
+    //Quand la souris est dessus
+    image.addEventListener('mousemove', e=>{
+        const percentX = e.offsetX  / image.offsetWidth * 100;
+        const percentY = e.offsetY  / image.offsetHeight * 100;
+        const rotateX = (percentY - 50) / 50 * 20;
+        const rotateY = (percentX - 50) / 50 * 20;
+        console.log(e.offsetY)
+        image.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    })
 
-articles.forEach(article => {
-  article.addEventListener('mousemove', e => {
-    const percentX = (e.clientX - article.offsetLeft) / article.offsetWidth * 100;
-    const percentY = (e.clientY - article.offsetTop) / article.offsetHeight * 100;
-    const rotateX = (percentY - 50) / 50 * 10;
-    const rotateY = (percentX - 50) / 50 * 10;
-    article.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-  });
-
-  article.addEventListener('mouseout', () => {
-    article.style.transform = 'none';
-  });
+    //Quand la souris n'est plus dessus
+    image.addEventListener('mouseout', ()=>{
+        image.style.transform = 'none';
+    })
 });
-
